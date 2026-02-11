8d27e6e81234e5a67e3024b87eb320f8beb6cc9b
user-admin123 <600001raj@gmail.com>
2026-02-11 12:57:16 +0530

    Update ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -273,7 +273,7 @@ export default function ContactPage() {
               <div className="rounded-lg overflow-hidden border border-border">
                 <iframe
                   title="GlossX Nail Studio Location"
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3495.5!2d150.4476!3d-26.9093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjbCsDU0JzMzLjUiUyAxNTDCsDI2JzUxLjQiRQ!5e0!3m2!1sen!2sau!4v1700000000000!5m2!1sen!2sau"
+                  src="https://maps.google.com/maps?q=38+Adams+St,+Tara+QLD+4421,+Australia&t=&z=16&ie=UTF8&iwloc=&output=embed"
                   width="100%"
                   height="250"
                   style={{ border: 0 }}
