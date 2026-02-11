a02913ea2a94c71934c130e0e96a3997c18f3ce4
user-admin123 <600001raj@gmail.com>
2026-02-11 13:16:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster position="top-right" />
+      <Toaster />
       <Sonner />
       <BrowserRouter>
         <ScrollToTop />
